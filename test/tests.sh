c8f4e350cc0e9343fe0060a5cbf238a34579313c
Yuping Zuo <zypA13510@users.noreply.github.com>
2026-04-13 20:20:00 +0800

    Update tests.sh

--- test/tests.sh
+++ test/tests.sh
@@ -2,26 +2,52 @@
 set -eu
 EXPECT=hello
 
+# The OOTB example uses host key generation at runtime, thus we need to skip host key checking.
+# This is not required in actual usage, as the user can simply save the host key in known_hosts on first connection.
+ssh_ootb() {
+	echo "ssh -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' bastion@bastion-ootb -p 2222 $1"
+}
+
+# Run ssh command in background and kill it after test
+run_ssh_test() {
+	SSH_CMD="$1"
+	TEST_CMD="$2"
+
+    eval "$SSH_CMD -N &"
+    SSH_PID=$!
+    trap "kill $SSH_PID" EXIT
+    sleep 2
+
+    eval "$TEST_CMD"
+
+    kill $SSH_PID
+    trap - EXIT
+}
+
 echo "====="
 echo "Should support local port mapping out of the box"
-ACTUAL=$(ssh -L 8080:app:5678 -N bastion@bastion-ootb -p 2222 && curl localhost:8080)
+ACTUAL=$(run_ssh_test "$(ssh_ootb "-L 8080:app:5678")" "curl http://localhost:8080")
 echo "Expect: ${EXPECT}"
 echo "Actual: ${ACTUAL}"
+[ "${ACTUAL}" = "${EXPECT}" ]
 
 echo "====="
 echo "Should support socks proxy out of the box"
-ACTUAL=$(ssh -D 1080 -N bastion@bastion-ootb -p 2222 && curl -x socks5h://localhost:1080 http://app:5678)
+ACTUAL=$(run_ssh_test "$(ssh_ootb "-D 1080")" "curl -x socks5h://localhost:1080 http://app:5678")
 echo "Expect: ${EXPECT}"
 echo "Actual: ${ACTUAL}"
+[ "${ACTUAL}" = "${EXPECT}" ]
 
 echo "====="
 echo "Should deny terminal access out of the box"
-ACTUAL=$(ssh bastion@bastion-ootb -p 2222 echo failed || echo passed)
+ACTUAL=$(eval "$(ssh_ootb "echo failed")" || echo passed)
 echo "Expect: passed"
 echo "Actual: ${ACTUAL}"
+[ "${ACTUAL}" = "passed" ]
 
 echo "====="
 echo "Should support hardening"
-ACTUAL=$(ssh -L 8080:app:5678 -N bastion@bastion-hardened -p 2222 && curl localhost:8080)
+ACTUAL=$(run_ssh_test "-L 8080:app:5678 bastion@bastion-hardened -p 2222" "curl http://localhost:8080")
 echo "Expect: ${EXPECT}"
 echo "Actual: ${ACTUAL}"
+[ "${ACTUAL}" = "${EXPECT}" ]
